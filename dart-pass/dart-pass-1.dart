215f91ebe20cd0c4fba70cfc8ec99bb61e955f4a
OMER RAAD <39926092+MROMER444@users.noreply.github.com>
2022-04-07 16:03:31 +0300

    Create dart-pass-1.dart

---
+++ dart-pass/dart-pass-1.dart
@@ -0,0 +1,9 @@
+main() {
+  var hour = DateTime.now().hour;
+  print(hour);
+}
+
+/* i chose var not const cause var it a variable
+and the const its a fixed value
+and we cant use a fixed value with datetime
+cause it doesn't get the current time. */
